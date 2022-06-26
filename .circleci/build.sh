e497ba471e7278261c1f81111cc9478aa04e76a4
Synchroz <juliyanto1071@student.untan.ac.id>
2022-06-27 02:03:09 +0700

    update build.sh and change gcc

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
 git clone --depth=1 https://github.com/Synchroz/kernel_xiaomi_santoni-4.9 santoni
-git clone https://github.com/Sepatu-Bot/arm64-gcc --depth=1 gcc
-git clone https://github.com/Sepatu-Bot/gcc-arm --depth=1 gcc32
+git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 gcc
+git clone https://github.com/mvaisakh/gcc-arm --depth=1 gcc32
 
 # Main
 KERNEL_NAME=Auguri # IMPORTANT ! Declare your kernel name
@@ -13,7 +13,7 @@ GCC_ROOTDIR=$(pwd)/gcc # IMPORTANT! Put your gcc directory here.
 GCC32_ROOTDIR=$(pwd)/gcc32 # IMPORTANT! Put your gcc32 directory here.
 export KBUILD_BUILD_USER=Synchroz # Change with your own name or else.
 export KBUILD_BUILD_HOST=Bloodedge # Change with your own hostname.
-GCC_VERSION = $(${GCC_ROOTDIR}/bin/aarch64-elf-gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
+GCC_VERSION="$("$GCC_ROOTDIR"/bin/aarch64-elf-gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 LLD_VERSION="$("$GCC_ROOTDIR"/bin/aarch64-elf-ld.lld --version | head -n 1)"
 COMPILER_STRING="$GCC_VERSION with $LLD_VERSION"
 IMAGE=$(pwd)/santoni/out/arch/arm64/boot/Image.gz-dtb
@@ -72,7 +72,7 @@ function compile() {
 	finerr
 	exit 1
    fi
-    git clone --depth=1 https://github.com/Synchroz/AnyKernel AnyKernel
+    git clone --depth=1 https://github.com/Synchroz/AnyKernel3 AnyKernel
 	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 
@@ -84,7 +84,7 @@ function push() {
         -F chat_id="-1001567409765" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi 4X (santoni)</b> | <b>$(${GCC_ROOTDIR}/bin/aarch64-linux-gnu-gcc-12.0.0 --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi 4X (santoni)</b> | <b>$(${GCC_ROOTDIR}/bin/aarch64-linux-gnu-gcc-13.0.0 --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 
 }
 # Fin Error
@@ -100,7 +100,7 @@ function finerr() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 ${KERNEL_NAME}-K4_9-${DEVICE_CODENAME}-${DATE}-GCC.zip *
+    zip -r9 ${KERNEL_NAME}-4.9-${DEVICE_CODENAME}-${DATE}-${GCC_VERSION}.zip *
     cd ..
 }
 check
