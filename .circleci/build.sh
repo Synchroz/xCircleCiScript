ff065cdbf17f355981210782192c70ca0a8da0e3
Synchroz <86562822+Synchroz@users.noreply.github.com>
2022-10-04 08:09:55 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
 git clone --depth=1 https://github.com/Synchroz/msm4.9_santonikernel santoni
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master gcc
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master gcc32
+git clone --depth=1 https://github.com/xyz-prjkt/xRageTC_build xRageTC # xRageTC set as Clang Default
+
 
 # Main
 KERNEL_NAME=Core # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/santoni # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=santoni # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=santoni_treble_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
-GCC_ROOTDIR=$(pwd)/gcc # IMPORTANT! Put your gcc directory here.
-GCC32_ROOTDIR=$(pwd)/gcc32 # IMPORTANT! Put your gcc32 directory here.
+CLANG_ROOTDIR=$(pwd)/xRageTC # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=Synchroz # Change with your own name or else.
 export KBUILD_BUILD_HOST=Bloodedge # Change with your own hostname.
-GCC_VERSION="$("$GCC_ROOTDIR"/bin/aarch64-elf-gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
-# LLD_VERSION="$("$GCC_ROOTDIR"/bin/aarch64-elf-ld.lld --version | head -n 1)"
-COMPILER_STRING="$GCC_VERSION with $LLD_VERSION"
+CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
+export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
 IMAGE=$(pwd)/santoni/out/arch/arm64/boot/Image.gz-dtb
 DATE=$(date +"%F-%S")
 START=$(date +"%s")
@@ -31,10 +30,8 @@ echo ================================================
 echo BUILDER NAME = ${KBUILD_BUILD_USER}
 echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
 echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
-echo GCC_VERSION = $(${GCC_ROOTDIR}/bin/aarch64-elf-gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
-echo GCC32_VERSION = $(${GCC32_ROOTDIR}//bin/arm-eabi-gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
-# echo LLD_VERSION = $(${GCC_ROOTDIR}/bin/ld.lld --version | head -n 1)
-echo GCC_ROOTDIR = ${GCC_ROOTDIR}
+echo TOOLCHAIN_VERSION = ${KBUILD_COMPILER_STRING}
+echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
 echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
 echo ================================================
 }
@@ -47,26 +44,21 @@ function compile() {
         -d chat_id="-1001567409765" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0AGCC VERSION : <code>${COMPILER_STRING}</code>"
+        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0AClang Version : <code>${KBUILD_COMPILER_STRING}</code>%0AClang Rootdir : <code>${CLANG_ROOTDIR}</code>%0AKernel Rootdir : <code>${KERNEL_ROOTDIR}</code>"
+
 
   cd ${KERNEL_ROOTDIR}
   make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-  make -j$(nproc) ARCH=arm64 O=out \
-    CROSS_COMPILE=${GCC_ROOTDIR}/bin/aarch64-elf- \
-    CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/bin/arm-eabi- \
-    AR=${GCC_ROOTDIR}/bin/aarch64-elf-gcc-ar \
-    AS=${GCC_ROOTDIR}/bin/aarch64-elf-as \
-#    NM=${GCC_ROOTDIR}/bin/aarch64-elf-gcc-nm \
-#    RANLIB=${GCC_ROOTDIR}/bin/aarch64-elf-gcc-ranlib \
-#    NM=${GCC_ROOTDIR}/bin/llvm-nm \
-    CC=${GCC_ROOTDIR}/bin/aarch64-elf-gcc \
-#    OBJCOPY=${GCC_ROOTDIR}/bin/aarch64-elf-objcopy \
-#    OBJCOPY=${GCC_ROOTDIR}/bin/llvm-objcopy \
-    OBJDUMP=${GCC_ROOTDIR}/bin/aarch64-elf-objdump \
-    OBJSIZE=${GCC_ROOTDIR}/bin/aarch64-elf-size \
-    READELF=${GCC_ROOTDIR}/bin/aarch64-elf-readelf \
-    STRIP=${GCC_ROOTDIR}/bin/aarch64-elf-strip 
-#    LD=${GCC_ROOTDIR}/bin/aarch64-elf-ld.lld
+make -j$(nproc) ARCH=arm64 O=out \
+    CC=${CLANG_ROOTDIR}/bin/clang \
+    AR=${CLANG_ROOTDIR}/bin/llvm-ar \
+    NM=${CLANG_ROOTDIR}/bin/llvm-nm \
+    OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
+    OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
+    STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
+    LD=${CLANG_ROOTDIR}/bin/ld.lld \
+    CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
